a46fd10b3a5598ae6b824fc23793ea0bd11e1e51
jk <jkpas30@gmail.com>
2016-03-15 17:01:29 +0200

    1

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-main.js
+$(document).ready(function(){
+
+	if (window.matchMedia('(max-width: 900px)').matches)
+	{
+	     $(".copyright").remove().insertAfter($(".footer_logo"));
+	};
+	if (window.matchMedia('(max-width: 768px)').matches)
+	{
+	     $(".left_sidebar").remove().insertAfter($(".content"));
+	};
+});
+
+

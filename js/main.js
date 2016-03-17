52ccb049c9d8977cd45ae24a0156bd6c4d975dad
jk <jkpas30@gmail.com>
2016-03-17 21:12:40 +0200

    -

--- js/main.js
+++ js/main.js
@@ -9,23 +9,23 @@ $(document).ready(function(){
 	     $(".left_sidebar").remove().insertAfter($(".content"));
 	};
 
-	// $("#form").submit(function() { //устанавливаем событие отправки для формы с id=form
- //        var form_data = $(this).serialize(); //собераем все данные из формы
- //        $.ajax({
-	//         type: "POST", //Метод отправки
-	//         url: "send.php", //путь до php фаила отправителя
-	//         data: form_data,
-	//         success: function() {
-	//             //код в этом блоке выполняется при успешной отправке сообщения
-	//             alert("Ваше сообщение отпрвлено!");
-	//        	}
-	//    	});
-	// });	
+	$("#form").submit(function() { //устанавливаем событие отправки для формы с id=form
+        var form_data = $(this).serialize(); //собераем все данные из формы
+        $.ajax({
+	        type: "POST", //Метод отправки
+	        url: "send.php", //путь до php фаила отправителя
+	        data: form_data,
+	        success: function() {
+	            //код в этом блоке выполняется при успешной отправке сообщения
+	            alert("Ваше сообщение отпрвлено!");
+	       	}
+	   	});
+	});	
 
 
 	$('#catalog').click(function(){  
         $.ajax({  
-            url: "page1.html",  
+            url: "/page1.html",  
             cache: false,  
             success: function(html){  
     	        $("#page_content").html(html);  
--- send.php
+++ send.php
@@ -1,5 +1,5 @@
-
-<!-- if((isset($_POST['name'])&&$_POST['name']!="")&&(isset($_POST['tel'])&&$_POST['tel']!="")&&(isset($_POST['email'])&&$_POST['email']!="")&&(isset($_POST['text'])&&$_POST['text']!="")){ //Проверка отправилось ли наше поля name и не пустые ли они
+<?php
+if((isset($_POST['name'])&&$_POST['name']!="")&&(isset($_POST['tel'])&&$_POST['tel']!="")&&(isset($_POST['email'])&&$_POST['email']!="")&&(isset($_POST['text'])&&$_POST['text']!="")){ //Проверка отправилось ли наше поля name и не пустые ли они
         $to = 'jkpas30@gmail.com'; //Почта получателя, через запятую можно указать сколько угодно адресов
         $subject = 'Обратный звонок'; //Загаловок сообщения
         $message = '
@@ -18,25 +18,6 @@
         $headers .= "From: Отправитель <admin@jkpas.xyz>\r\n"; //Наименование и почта отправителя
         mail($to, $subject, $message, $headers); //Отправка письма с помощью функции mail
         echo 'Спасибо! Ваше письмо отправлено.'; 
-} -->
+}
 
-
-<?php
-
-$name = $_POST['name'];
-$tel = $_POST['tel'];
-$email = $_POST['email'];
-
-$to = "jkpas30@gmail.com";
-$subject = "От посетителя сайта";
-$text =  "Написал(а): $name\n
-          Телефон: $tel\n   
-          Контактный email - $email\n";
-
-$headers = "Content-type: text/html; charset=utf-8\r\n";
-$headers .= "MIME-Version: 1.0\r\n";
-$sending = mail($to, $subject, $text, $headers);
-
-if($sending) echo "Письмо отправлено.";
-?>
 
